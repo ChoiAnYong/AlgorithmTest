3bc4d71e8397acf928eb8e21d627107fbea9da2c
ChoiAnYong <dksdyd78@naver.com>
2023-11-26 23:19:39 +0900

    first

---
+++ AlgorithmTest/ex1.swift
@@ -0,0 +1,20 @@
+//
+//  ex1.swift
+//  AlgorithmTest
+//
+//  Created by 최안용 on 11/16/23.
+//
+import Foundation
+
+let n = Int(readLine()!)!
+let input = readLine()!.split(separator: " ").map {
+    Int(String($0))!
+}
+let num = Int(readLine()!)!
+var count: Int = 0
+for i in input {
+    if i == num {
+        count += 1
+    }
+}
+print(count)
---
+++ AlgorithmTest/main.swift
@@ -0,0 +1,9 @@
+//
+//  main.swift
+//  AlgorithmTest
+//
+//  Created by 최안용 on 11/16/23.
+//
+//단어 개수
+let sentence = readLine()!.split(separator: " ")
+print(sentence.count)
