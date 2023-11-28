97108c85c96efe07f3fcc00c5cd66d39fdf5d68d
ChoiAnYong <dksdyd78@naver.com>
2023-11-29 00:30:50 +0900

    ex4

--- AlgorithmTest/main.swift
+++ AlgorithmTest/main.swift
@@ -2,33 +2,36 @@
 //  main.swift
 //  AlgorithmTest
 //
-//  Created by 최안용 on 11/27/23.
+//  Created by 최안용 on 11/28/23.
 //
 
 import Foundation
 
-var alpha = [Int](repeating: 0, count: 26)//배열 크기를 26으로 하고 0으로 초기화
-let sentence = readLine()!.uppercased().map{String($0)}//입력 받은 문자를 대문자로 변환하여 받음
+let num = Int(readLine()!)!
+var list: [Int] = [Int]()
 
-for ch in sentence {
-    let index: Int = Int(UnicodeScalar(ch)!.value) - Int(UnicodeScalar("A").value)//Int(UnicodeSccalar()!.value)를 쓰면 아스키코드값을 알 수 있다.
-    alpha[index] += 1
-}
-
-var maxIndex: Int = 0
-var check: Bool = false
-// 배열의 인덱스를 알기 위해 .enumerated()를 사용한다. 인덱스 $0.offset, 값 $0.element $0.0 $0.1로 줄여쓸 수 있다.
-alpha.enumerated().forEach {
-    if alpha[maxIndex] < $0.1 {
-        maxIndex = $0.0
+for _ in 0..<num {
+    let menu = readLine()!.split(separator: " ").map {
+        Int(String($0))!
+    }
+    
+    switch menu[0] {
+    case 1:
+        list.append(menu[1])
+    case 2:
+        if list.isEmpty == true {
+            print("-1")
+        } else {
+            print(list.last!)
+            list.removeLast()
+        }
+    case 3:
+        print(list.count)
+    case 4:
+        list.isEmpty == true ? print("1") : print("0")
+    case 5:
+        list.isEmpty == true ? print("-1") : print(list.last!)
+    default: break
     }
 }
 
-alpha.enumerated().forEach {
-    if alpha[maxIndex] == $0.1 && $0.0 != maxIndex {
-        check = true
-    }
-}
-
-let result = String(UnicodeScalar(65 + maxIndex)!)//String(UnicodeScalar()!)를 쓰면 아스키코드를 문자로 변환할 수 있다.
-check ? print("?") : print(result)
