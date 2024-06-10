79dfa9b7ae6a7b8cb8312ddc8baa3cc2c6726ee5
ChoiAnYong <dksdyd78@naver.com>
2024-06-10 22:31:32 +0900

    feat: 백준 1976번

---
+++ AlgorithmTest/1976_여행가자.swift
@@ -0,0 +1,43 @@
+//
+//  main.swift
+//  AlgorithmTest
+//
+//  Created by 최안용 on 3/15/24.
+//
+import Foundation
+
+let N = Int(readLine()!)! // 도시의 수
+let M = Int(readLine()!)! // 여행 계획에 속한 도시들의 수
+
+var list: [[Int]] = []
+var answer: Bool = true
+
+for _ in 0..<N {
+    let input = readLine()!.split(separator: " ").map({Int($0)!})
+    list.append(input)
+}
+
+var plan = readLine()!.split(separator: " ").map({Int($0)!})
+
+var check = Array(repeating: false, count: N)
+
+func dfs(index : Int) {
+    check[index] = true
+    
+    for i in 0..<N {
+        if list[index][i] == 1 && check[i] == false {
+            dfs(index: i)
+        }
+    }
+}
+
+dfs(index: plan[0]-1)
+
+for p in plan {
+    if !check[p-1] {
+        answer = false
+        break
+    }
+}
+
+print(answer ? "YES":"NO")
--- AlgorithmTest/main.swift
+++
@@ -1,8 +0,0 @@
-//
-//  main.swift
-//  AlgorithmTest
-//
-//  Created by 최안용 on 3/15/24.
-//
-
-import Foundation
