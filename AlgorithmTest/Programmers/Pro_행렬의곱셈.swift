5b4e1994daa2efc7aeaf3c5355a7f81ab3689d08
ChoiAnYong <dksdyd78@naver.com>
2025-10-22 15:21:20 +0900

    feat: 프로그래머스(2) 행렬의 곱셈

---
+++ AlgorithmTest/Programmers/Pro_행렬의곱셈.swift
@@ -0,0 +1,22 @@
+//
+//  Pro_행렬의곱셈.swift
+//  AlgorithmTest
+//
+//  Created by 최안용 on 10/22/25.
+//
+
+import Foundation
+
+func solution(_ arr1:[[Int]], _ arr2:[[Int]]) -> [[Int]] {
+    var result = Array(repeating: Array(repeating: 0,count: arr2[0].count), count: arr1.count)
+    
+    for i in 0..<arr1.count {
+        for j in 0..<arr2[0].count {
+            for k in 0..<arr1[0].count {
+                result[i][j] += arr1[i][k] * arr2[k][j]
+            }
+        }
+    }
+    
+    return result
+}
